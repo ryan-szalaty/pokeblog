e01681bf20ed498643c6bdebb70af4aa14265d60
ryan-szalaty <ryan.szalaty@gmail.com>
2022-07-05 07:58:18 +0900

    First commit.

---
+++ src/components/pokedex.jsx
@@ -0,0 +1,10 @@
+function Pokedex() {
+    return (
+        <div>
+            <h2>This is the pokedex section!</h2>
+            <p>The pokedex will go here!</p>
+        </div>
+    )
+}
+
+export default Pokedex;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+//Page imports
+import Homepage from "./pages/home";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage/>}/>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
